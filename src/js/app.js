76abbd6393668b6d7fd3c205612c6a3b22fdb0ae
Zach Wolf <hello@zachwolf.com>
2014-11-29 18:55:57 -0600

    adds Shatter creation

--- src/js/app.js
+++ src/js/app.js
@@ -5,12 +5,30 @@
 
   require('polyfill')
 
+  // probably better Math.random
+  // https://github.com/davidbau/seedrandom
+  Math.seedrandom()
+
   var Shatter = require('shatter')
+    , Chain = require('class/chain.class')
 
   $(function(){
     /**
      * kick everything off now that the document is ready
      */
-    console.log('hi!', new Shatter);
+    var $stage  = $('canvas')
+      , ctx     = new Chain($stage.get(0).getContext('2d'))
+      , stage   = {
+          $       : $stage
+        , width   : $stage.prop('width')
+        , height  : $stage.prop('height')
+        , centerX : $stage.prop('width')  / 2
+        , centerY : $stage.prop('height') / 2
+        }
+      , shatter = new Shatter({
+          x: stage.width  / 3
+        , y: stage.height / 3
+        , stage: stage
+        })
   })
 }(jQuery, window))
